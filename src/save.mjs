f37a2d2418a5501cea95d531ec947067836c482b
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 13:52:17 -0500

    Update save.mjs

--- src/save.mjs
+++ src/save.mjs
@@ -1,1 +1,15 @@
+import program from '../config.js';
+const fs = program.fs;
+const data = program.data;
 
+const save = () => {
+  let loottable = data.get("session");
+  
+  fs.writeFileSync(`../tables/${loottable.name}.json`, JSON.stringify(loottable, null, 4), (err) => {
+    if(err) console.warn(err);
+  });
+  
+  console.info("Current loot table saved.");
+}
+
+export default save;
