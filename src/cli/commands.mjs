6e64eedb587527870f45c42076e09fbd2a63b229
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 12:56:31 -0500

    Added more commands

--- src/cli/commands.mjs
+++ src/cli/commands.mjs
@@ -15,6 +15,10 @@ import init from '../init.mjs';
 import open from '../open.mjs';
 import read from '../read.mjs';
 import reset from '../reset.mjs';
+import rename from '../rename.mjs';
+import save from '../save.mjs';
+import backup from '../backup.mjs';
+import restore from '../restore.mjs';
 
 // setup commander
 cmd
@@ -44,6 +48,78 @@ cmd
   .description("Adds a new loot table object.")
   .action((name, weight, data) => {
         add(name, weight, data);
+});
+
+cmd
+  .command("remove <entry-name>")
+  .alias("rm")
+  .description("Removes a loot table object.")
+  .action((name) => {
+        remove(name);
+});
+
+cmd
+  .command("edit <entry-name> <new-name> <weight> <object-data>")
+  .alias("edit")
+  .description("Edits a loot table object. (Use ~ to ignore a section)")
+  .action((name, newName, weight, data) => {
+      edit(name, newName, weight, data);
+});
+
+cmd
+  .command("finish")
+  .alias("f")
+  .description("Saves and closes the current loot table.")
+  .action(() => {
+      finish();
+});
+
+cmd
+  .command("init <name>")
+  .alias("i")
+  .description("Creates a new loot table.")
+  .action((name) => {
+      init(name);
+});
+
+cmd
+  .command("open <name>")
+  .alias("o")
+  .description("Opens a loot table.")
+  .action((name) => {
+      open(name);
+});
+
+cmd
+  .command("read")
+  .alias("read")
+  .description("Prints the current loot table to the console.")
+  .action(() => {
+      read();
+});
+
+cmd
+  .command("reset")
+  .alias("reset")
+  .description("Resets the current loot table.")
+  .action(() => {
+      reset();
+});
+
+cmd
+  .command("rename <name>")
+  .alias("rename")
+  .description("Renames the current loot table.")
+  .action((name) => {
+      rename(name);
+});
+
+cmd
+  .command("save")
+  .alias("s")
+  .description("Saves the current loot table.")
+  .action(() => {
+      save();
 });
 
 export default cmd;
