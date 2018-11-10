e9f23d6b7814067c94039b0c37cf1468cba0cb86
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-09 21:50:18 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,21 +1,3 @@
 import cmd from './src/cli/commands.mjs';
 
-import cancel from '../cancel.mjs';
-import close from '../close.mjs';
-import add from '../add.mjs';
-import remove from '../remove.mjs'; 
-import edit from '../edit.mjs';
-import finish from '../finish.mjs';
-import init from '../init.mjs';
-import open from '../open.mjs';
-import read from '../read.mjs';
-import reset from '../reset.mjs';
-
-let table = {
-  "entry-count": 0,
-  "entries": [
-    
-    ],
-};
-
 cmd.parse(process.argv);
