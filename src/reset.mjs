947147b506557f500ca649d3ffcc7ea1107d6381
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 15:11:57 -0500

    Update reset.mjs

--- src/reset.mjs
+++ src/reset.mjs
@@ -1,5 +1,24 @@
+import program from '../config.js';
+const data = program.data;
+
 const reset = () => {
+  let loottable = data.get("session");
   
+  let table = {
+    "name": loottable.name;
+    "item-count": 0,
+     "items": {
+      "Null": {
+        "name": "Null",
+        "weight": 0,
+        "data": {},
+        },
+      },
+  };
+  
+  data.set("session", table);
+  
+  console.info("Current loot table reset");
 };
 
 export default reset;
