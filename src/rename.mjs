a50fbdf7a1048042952d7d9eee84547297590138
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 14:40:12 -0500

    Update rename.mjs

--- src/rename.mjs
+++ src/rename.mjs
@@ -1,1 +1,14 @@
+import program from '../config.js';
+const data = program.data;
 
+const rename = (name) => {
+  let loottable = data.get("session");
+  
+  if(!name) return console.info("No name provided.");
+  
+  loottable.name = name;
+  
+  data.set("session", loottable);
+  
+  console.info("Current loot table renamed.");
+};
