bb7127cc7279b26ff4cf65d242eac2ee1fa119e7
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 12:10:40 -0500

    Update open.mjs

--- src/open.mjs
+++ src/open.mjs
@@ -1,5 +1,11 @@
-const open = () => {
-  
+import program from '../config.js';
+const fs = program.fs;
+const data = program.data;
+
+const open = (name) => {
+  let loottable = JSON.parse(fs.readFileSync(`../tables/${name}.json`));
+  data.set("session", loottable);
+  console.info("Loot table opened.");
 };
 
 export default open;
