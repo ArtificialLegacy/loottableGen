da18f3b3c0ab294a93880a0cc2c9ee8f2b185a86
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 12:21:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,19 @@
 const commander = require("commander");
+const Enmap = require('enmap');
+const EnmapLevel = require('enmap-level');
+const fs = require("fs");
 
-modules.exports = commander;
+const data = new Enmap({
+  provider: new EnmapLevel({
+    name: 'data' 
+  })
+});
+
+const program = {
+  "cmd": commander,
+  "data": data,
+  "fs": fs,
+  "version": require("./package.json").version,
+};
+
+modules.exports = program;
