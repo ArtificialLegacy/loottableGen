45d17f61208295ee456a344bab6ecf500596ef3f
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 10:56:29 -0500

    Update close.mjs

--- src/close.mjs
+++ src/close.mjs
@@ -1,5 +1,22 @@
+import program from '../config.js';
+const fs = program.fs;
+const data = program.data;
+
 const close = () => {
+    let table = {
+    "name": "Null",
+    "item-count": 0,
+    "items": [
+      {
+        "name": "Null",
+        "weight": 0,
+        "data": {},
+      },
+    ],
+  };
   
+  data.set("session", table);
+  console.info("Current loot table closed.");
 };
 
 export default close;
