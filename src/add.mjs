6af57c00e297b5c74666a623c6c4c9f5c86a1fce
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 14:53:27 -0500

    Update add.mjs

--- src/add.mjs
+++ src/add.mjs
@@ -1,5 +1,16 @@
+import program from '../config.js';
+const data = program.data;
+
 const add = (name, weight, data) => {
+  let loottable = data.get("session");
   
+  loottable.entries[name] = {
+    "name": name,
+    "weight": parseInt(weight),
+    "entry-data": data,
+  };
+  
+  data.set("session", loottable);
 };
 
 export default add;
