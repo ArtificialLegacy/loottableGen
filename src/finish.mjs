44af6fdb6bf6d54e4d7e7e4c17e4bc12122ce8a9
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 11:59:19 -0500

    Update finish.mjs

--- src/finish.mjs
+++ src/finish.mjs
@@ -1,5 +1,31 @@
+import program from '../config.js';
+const fs = program.fs;
+const data = program.data;
+
 const finish = () => {
   
+  let loottable = data.get("session");
+  fs.writeFileSync(`../tables/${loottable.name}.json`, JSON.stringify(loottable, null, 4), (err) => {
+    if(err) console.warn(err);
+  });
+  
+  console.info("Loot table saved.");
+  
+  let table = {
+    "name": "Null",
+    "item-count": 0,
+    "items": [
+      {
+        "name": "Null",
+        "weight": 0,
+        "data": {},
+      },
+    ],
+  };
+  
+  data.set("session", table);
+  console.info("Current loot table closed.");
+
 };
 
 export default finish;
