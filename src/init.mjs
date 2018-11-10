d2716aa2ec05a79a39a81b14317b2f86a8ea5887
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-09 22:05:41 -0500

    Update init.mjs

--- src/init.mjs
+++ src/init.mjs
@@ -1,5 +1,29 @@
-const init = () => {
+import program from '../config.js';
+const fs = program.fs;
+const data = program.data;
+
+const init = (name) => {
+  let table = {
+    "name": name,
+    "item-count": 0,
+    "items": [
+      {
+        "name": "Null",
+        "weight": 0,
+        "data": {},
+      },
+    ],
+  };
   
+  data.set("session", table);
+  
+  let check = JSON.parse(fs.readFileSync(`../tables/${name}.json`));
+  
+  if(check[name]) throw("This loot table already exists. Try using the open command.");
+  
+  fs.writeFileSync(`../tables/${name}.json`, JSON.stringify(table, null, 4), (err) => {
+    if(err) console.warn(err);
+  });
 };
 
 export default init;
