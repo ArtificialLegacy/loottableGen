ba01275f91e19be6ba40f2bb15f6f5264dc1bbc8
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 14:09:44 -0500

    Update backup.mjs

--- src/backup.mjs
+++ src/backup.mjs
@@ -1,1 +1,13 @@
+import program from '../config.js';
+const data = program.data;
 
+const backup = (name) => {
+  let loottable = data.get("session");
+  let backups = data.get("backups");
+  
+  if(!backups) backups = {};
+  
+  backups[name] = loottable;
+  
+  console.info("Created a backup of the current loot table.");
+};
