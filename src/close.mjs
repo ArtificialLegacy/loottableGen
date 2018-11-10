9ffe69bec7166cb8b4b1e6f2b898fa1158481bec
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 13:30:45 -0500

    Update close.mjs

--- src/close.mjs
+++ src/close.mjs
@@ -4,15 +4,6 @@ const data = program.data;
 
 const close = () => {
     let table = {
-    "name": "Null",
-    "item-count": 0,
-    "items": [
-      {
-        "name": "Null",
-        "weight": 0,
-        "data": {},
-      },
-    ],
   };
   
   data.set("session", table);
