76c3c489a828e6333c4e3bb86bfbb0321b0ceb8a
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 15:34:21 -0500

    Update edit.mjs

--- src/edit.mjs
+++ src/edit.mjs
@@ -1,5 +1,22 @@
-const edit = () => {
+import program from '../config.js';
+const data = program.data;
+
+const edit = (name, newName, weight, entrydata) => {
+  if(!name) return console.info("No entry name provided.");
   
+  let loottable = data.get("session");
+  
+  if(!newName || newName == "~") newName = loottable.items[name].name;
+  if(!weight || weight == "~") weight = loottable.items[name].weight;
+  if(!entrydata || entrydata == "~") entrydata = loottable.items[name].entrydata;
+  
+  loottable.items[name].name = newName;
+  loottable.items[name].weight = weight;
+  loottable.items[name].entrydata = entrydata;
+  
+  data.set("session", loottable);
+  
+  console.info("Editted loot table object.");
 };
 
 export default edit;
