cf91ee797b08474d2e9ddcf3d63ed890db3e84f5
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 15:01:33 -0500

    Update remove.mjs

--- src/remove.mjs
+++ src/remove.mjs
@@ -1,5 +1,13 @@
-const remove = () => {
+import program from '../config.js';
+
+const remove = (name) => {
+  let loottable = data.get("session");
   
+  delete loottable.entries[name];
+  
+  data.set("session", loottable);
+  
+  console.info("Loot table object removed.");
 };
 
 export default remove;
