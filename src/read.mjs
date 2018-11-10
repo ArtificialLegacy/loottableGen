6e5ad2b9d42eb9771d31e8783e36039cdd29d9c7
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 14:37:04 -0500

    Update read.mjs

--- src/read.mjs
+++ src/read.mjs
@@ -1,5 +1,10 @@
+import program from '../config.js';
+const data = program.data;
+
 const read = () => {
+  let loottable = data.get("session");
   
+  console.dir(loottable);
 };
 
 export default read;
