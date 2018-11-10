c7bf747c1ef16b68156466caf52c24f338595a22
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 14:43:39 -0500

    Update backup.mjs

--- src/backup.mjs
+++ src/backup.mjs
@@ -2,6 +2,9 @@ import program from '../config.js';
 const data = program.data;
 
 const backup = (name) => {
+  
+  if(!name) return console.info("No name value provided.");
+  
   let loottable = data.get("session");
   let backups = data.get("backups");
   
