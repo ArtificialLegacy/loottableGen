8db1542aa2d643e4bcc93edf0d4c6d05db819b2e
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 14:20:57 -0500

    Update restore.mjs

--- src/restore.mjs
+++ src/restore.mjs
@@ -1,1 +1,11 @@
+import program from '../config.js';
+const data = program.data;
 
+const restore = (name) => {
+  let backups= data.get("backups");
+  let loottable = backups[name];
+  
+  if(!loottable) return console.info("Backup does not exist!");
+  
+  data.set("session", loottable);
+};
